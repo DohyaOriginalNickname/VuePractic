1da33292270403928e75215a81e7e823a80015c4
DohyaOriginalNickname <tsareg.n@gmail.com>
2022-03-03 21:38:35 +0600

    registration routs

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,56 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+import Orders from '@/components/Users/Orders'
+import NewAd from '@/components/Ads/NewAd'
+import AdList from '@/components/Ads/AdList'
+import Ad from '@/components/Ads/Ad'
+
+
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path:'/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path:'/registration',
+    name: 'Reg',
+    component: Registration,
+  },
+  {
+    path:'/orders',
+    name: 'Orders',
+    component: Orders,
+  },
+  {
+    path:'/new',
+    name: 'NewAd',
+    component: NewAd,
+  },
+  {
+    path:'/List',
+    name: 'AdList',
+    component: AdList,
+  },
+  {
+    path:'/ad/:id',
+    name: 'Ad',
+    component: Ad,
+    children:[
+
+    ]
+  }
 ]
 
 const router = new VueRouter({
